620e440100326cb4cf8b98d6dc80c4ed58fccad7
OguzSahin58 <oguz.sahin.20742@ozu.edu.tr>
2022-12-24 17:31:51 +0300

    Update MineSweeper.java
    Database connection set

--- 320/src/p2/MineSweeper.java
+++ 320/src/p2/MineSweeper.java
@@ -18,6 +18,40 @@ private static JLabel picture;
 private static String username;
 private static JFrame frame2;
 
+private static final String DB_USER = "root";
+private static final String DB_PASS = "admin";
+private static final String DB_NAME = "mines";
+private static final String URL= "jdbc:mysql://localhost:3306/"+ DB_NAME+ "?useSSL=false";
+private static Connection connection = null;
+
+public static void establishConnection() {
+	try {
+		connection = DriverManager.getConnection(URL,DB_USER,DB_PASS);
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
+public static void closeConnection() {
+	try {
+		connection.close();
+	}catch(SQLException e){
+		e.printStackTrace();
+	}
+}
+
+public static void main(String[] args) {
+	try {
+		establishConnection();
+		Statement statement = connection.createStatement();	
+		
+	} catch (SQLException e) {
+		e.printStackTrace();			
+	}
+	finally {
+		closeConnection();	
+	}
+}
 
 
 
