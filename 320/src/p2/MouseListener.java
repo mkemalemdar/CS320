96aea0d9ed083ca75e8d8a9d6dc255e70accb27a
feyzimerterim <81638136+feyzimerterim@users.noreply.github.com>
2022-12-24 18:09:51 +0300

    Update MouseListener.java
    fixed some typos

--- 320/src/p2/MouseListener.java
+++ 320/src/p2/MouseListener.java
@@ -2,17 +2,19 @@ package p2;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.*;
+import java.util.*;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-public class MouseListener extends MouseAdapter {
+
+public class MouseListener extends MouseAdapter  {
 private MineGrid grid;
 private int row;
 private int col;
-
 public MouseListener(int x,int y, MineGrid g) {
 	row=x;
 	col=y;
@@ -27,31 +29,30 @@ public MouseListener(int x,int y, MineGrid g) {
 		
 		if(!MineSweeperGUI.getFlags()[row][col] && !MineSweeperGUI.getOpenedCells()[row][col] && grid.isMINE(row, col)) { 
 		MineSweeper.mineCountDown();
-		Icon icon = new ImageIcon("src\p2\\flag.png");
+		Icon icon = new ImageIcon("src\\p2\\flag.png");
 		  button.setIcon(icon);
 		  MineSweeperGUI.getFlags()[row][col]=true;
 		  	}  
 		else if(!MineSweeperGUI.getFlags()[row][col] && !MineSweeperGUI.getOpenedCells()[row][col] ) { 
 		
-			Icon icon = new ImageIcon("src\p2\\flag.png");
+			Icon icon = new ImageIcon("src\\p2\\flag.png");
 			  button.setIcon(icon);
 			  MineSweeperGUI.getFlags()[row][col]=true;
 			  	}  
 		else if (!MineSweeperGUI.getOpenedCells()[row][col] && grid.isMINE(row, col)) {
 		MineSweeper.mineCountUp();
-				Icon icon1 = new ImageIcon("src\p2\\button.png");
+				Icon icon1 = new ImageIcon("src\\p2\\button.png");
 				button.setIcon(icon1);
 			 MineSweeperGUI.getFlags()[row][col]=false;
 	} 	else if(!MineSweeperGUI.getOpenedCells()[row][col] ){
 
-		Icon icon1 = new ImageIcon("src\p2\\button.png");
+		Icon icon1 = new ImageIcon("src\\p2\\button.png");
 		button.setIcon(icon1);
 		  MineSweeperGUI.getFlags()[row][col]=false;
 
 	}
 }
-
-	if (MineSweeper.getMineCount()==0) {
+if (MineSweeper.getMineCount()==0) {
 	int time = MineSweeper.getTime();
 	String name = MineSweeper.getName();
 	JOptionPane.showMessageDialog(null, "You have completed the game in "+ time + " seconds!");
@@ -59,6 +60,7 @@ public MouseListener(int x,int y, MineGrid g) {
 
 	 
 } else return;
-	}	
+	
+}
 	
 }
